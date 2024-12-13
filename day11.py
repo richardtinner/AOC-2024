699346585f2b71bddc95d9e32be779695f9b5607
Richard Tinner <richardtinner@yahoo.co.uk>
2024-12-13 17:14:38 +0000

    day 11 part2

--- day11.py
+++ day11.py
@@ -1,5 +1,6 @@
 stones= []
-import copy
+import time
+from collections import Counter
 
 def blink(stones):
     new_stones = []
@@ -14,13 +15,30 @@ def blink(stones):
             new_stones.append(s * 2024)
     return new_stones
 
-def list_part2stones(cs, os):
-    s = os
-    for key, count in cs.items():
-        if count > 0:
-            for i in range(0, count):
-                s.append(key)
-    return s
+def fast_mutate(state): 
+    counter = Counter(state) 
+    new_state = Counter(counter) 
+    res = 0
+    for stone, count in counter.items(): 
+        if count == 0: continue 
+        if stone == 0: 
+            new_state[0]-=count 
+            new_state[1]+=count 
+        elif len(str(stone)) %2 == 0: 
+            digits = str(stone) 
+            ld = int(digits[:len(digits)//2]) 
+            rd = int(digits[len(digits)//2:]) 
+            new_state[stone]-=count 
+            new_state[ld]+=count 
+            new_state[rd]+=count 
+        else: 
+            new_state[stone*2024]+=count 
+            new_state[stone]-=count 
+    
+    nz = [(n, v) for n, v in new_state.items() if v>0]
+    res = sum([v[1] for v in nz])
+    
+    return new_state, res
 
 with open("day11-data.txt") as f:
 
@@ -28,8 +46,9 @@ with open("day11-data.txt") as f:
 
 # Day 1
 #print('0', len(stones), stones)
+t1 =time.time()
 part1 = []
-original_stones = copy.deepcopy(stones)
+original_stones = stones
 for n in range (0, 25):
     stones = blink(stones)
     stones.sort()
@@ -37,72 +56,19 @@ for n in range (0, 25):
     if n+1 == 6:
         print(stones)
 
-print("Day11, part 1 = ", len(stones))
+t2 =time.time()
 
-stones = copy.deepcopy(original_stones)
+print("Day11, part 1 = ", len(stones), " time = ", t2-t1)
+
+stones = original_stones
 
 # Day 2
-precalc_stones = {}
-core_stones = {}
-other_stones = []
-for i in range(0, 1000000):
-    precalc_stones[i] = blink([i])
-    core_stones[i] = 0
-for i in range(2024, 2024*10, 2024):
-    precalc_stones[i] = blink([i])
-    core_stones[i] = 0
-
-empty_core_stones = copy.deepcopy(core_stones)
-for s in stones:
-    if s in core_stones:
-        core_stones[s] += 1
-    else:
-        other_stones.append(s)
-
-
+t1 =time.time()
+state = Counter(stones)
+tot = 0
 for n in range (0, 75):
-    # first process core stones
-    #if n+1 == 6:
-    #    print("x")
-    new_core_stones = copy.deepcopy(empty_core_stones)
-    new_other_stones = []
-    for key, count in core_stones.items():
-        #if key == 10120:
-        #    print("y")
-        if count > 0:
-            for s in precalc_stones[key]:
-                if s in new_core_stones:
-                    new_core_stones[s] += count
-                else:
-                    for i in range(0, count):
-                        new_other_stones.append(s)
+    state, tot = fast_mutate(state)
+    print(n+1, tot)
 
-
-        
-    # then process other stones
-    other_stones = blink(other_stones)
-    for s in other_stones:
-        if s in new_core_stones:
-            new_core_stones[s] += 1
-        else:
-            new_other_stones.append(s)
-    
-    core_stones = copy.deepcopy(new_core_stones)
-    other_stones = copy.deepcopy(new_other_stones)
-    cs = copy.deepcopy(new_core_stones)
-    os = copy.deepcopy(new_other_stones)
-
-    s = list_part2stones(cs, os)
-    s.sort()
-    print(n+1, len(s))
-    #if n+1 == 6:
-    #    print(s)
-
-
-
-part2_len = len(other_stones)
-for key, count in core_stones.items():
-    if count > 0:
-        part2_len += count
-
-print("Day11, part 2 = ", part2_len)
+t2 =time.time()
+print("Day11, part 2 = ", tot, " time = ", t2-t1)
