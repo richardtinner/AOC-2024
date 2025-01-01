c4e29a04515f36ed31a0a84b539559dc75361315
Richard Tinner <richardtinner@yahoo.co.uk>
2025-01-01 17:06:16 +0000

    part 1 tidy up and start looking at part 2

--- day17.py
+++ day17.py
@@ -1,10 +1,6 @@
-registerA = 0
-registerB = 0
-registerC = 0
-program = []
-ptr = 0
+import itertools
 
-def get_combo_operand(lo):
+def get_combo_operand(lo, registerA, registerB, registerC):
     co = lo
     if lo == 4:
         co = registerA
@@ -18,47 +14,59 @@ def get_combo_operand(lo):
 
     return co
 
-def process_instr(ptr):
-    global registerA
-    global registerB
-    global registerC
-    instr = program[ptr]
-    operand = program[ptr + 1]
-    combo_operand = get_combo_operand(operand)
-    
+def run_program(registerA, registerB, registerC, program):
+    ptr = 0
 
-    ptr += 2
-    if instr == 0:
-        registerA = int(registerA / 2 ** combo_operand)
-    elif instr == 1:
-        registerB = registerB ^ operand
-    elif instr == 2:
-        registerB = combo_operand % 8
-    elif instr == 3:
-        if registerA != 0:
-            ptr = operand
-    elif instr == 4:
-        registerB = registerB ^ registerC
-    elif instr == 5:
-        print(combo_operand % 8, end = '')
-        print(',', end='')
-    elif instr == 6:
-        registerB = int(registerA / 2 ** combo_operand)
-    elif instr == 7:
-        registerC = int(registerA / 2 ** combo_operand)
+    while ptr < len(program):
+        instr = program[ptr]
+        operand = program[ptr + 1]
+        combo_operand = get_combo_operand(operand, registerA, registerB, registerC)
+        ptr += 2
+        if instr == 0:
+            registerA = int(registerA / 2 ** combo_operand)
+        elif instr == 1:
+            registerB = registerB ^ operand
+        elif instr == 2:
+            registerB = combo_operand % 8
+        elif instr == 3:
+            if registerA != 0:
+                ptr = operand
+        elif instr == 4:
+            registerB = registerB ^ registerC
+        elif instr == 5:
+            print(combo_operand % 8, end = '')
+            print(',', end='')
+        elif instr == 6:
+            registerB = int(registerA / 2 ** combo_operand)
+        elif instr == 7:
+            registerC = int(registerA / 2 ** combo_operand)
 
-    return ptr
+    print()
+    return 
+
 
 with open("day17-data.txt") as f:
+    # Read the file
     lines = f.readlines()
     registerA = int(lines[0].split(':')[1].strip('\n'))
     registerB = int(lines[1].split(':')[1].strip('\n'))
     registerC = int(lines[2].split(':')[1].strip('\n'))
     program = [int(c) for c in lines[4].split(':')[1].split(',')]
-    print(program)
-
-    while ptr < len(program):
-        ptr = process_instr(ptr)
     
-    print()
+    # Solve part 1
+    print("Day17 part 1")
+    print("Program = ", program)
+    print("Solution = ", end ='')
+
+    run_program(registerA, registerB, registerC, program)
+    
+    # Part 2 
+    sa = [0o7777777777777777, 0o1000000000000000, 0o777777777777777]
+    for a in sa:
+        print(a, oct(a))
+        run_program(a, registerB, registerC, program)
+
+    print(0o7777777777777777 - 0o1000000000000000)
+
+
     
