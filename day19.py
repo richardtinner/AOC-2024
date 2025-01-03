1d26fd4449d584e7d64e382f2e5cfafb2d94486a
Richard Tinner <richardtinner@yahoo.co.uk>
2025-01-03 11:14:19 +0000

    day19 part 2 solution

--- day19.py
+++ day19.py
@@ -16,10 +16,6 @@ def check_design2(design, sum):
     for t in towels:
         if design.startswith(t):
             if len(t) == len(design):
-                if sum % 10000 == 0:
-                    print('.',end='')
-                if sum % 100000 == 0:
-                    print(sum)
                 return sum+1
             else:
                 sum = check_design2(design[len(t):], sum)
@@ -30,27 +26,16 @@ with open("day19-data.txt") as f:
     lines = f.readlines()
     towels = [t.strip(' ') for t in lines[0].strip('\n').split(',')]
     designs = [d.strip('\n') for d in lines[2:]]
-    print(towels)
 
     sum = 0
     for d in designs:
-        print("Checking ", d, end=' ')
-        if check_design(d) == 1:
-            sum += 1
-            print("found")
-        else:
-            print("impossible")
-    
+        sum += check_design(d)
     print("Day 19 part 1 = ", sum)
+    
     sum = 0
     for d in designs:
-        print("Checking ", d, end=' ')
-        found = check_design2(d, 0)
-        if found != 0:
-            sum += found
-            print("\nfound ", found)
-        else:
-            print("impossible")
+        sum +=  check_design2(d, 0)
+        
     print("Day 19 part 2 = ", sum)
 
 
