28cf0a8bbe4ef6db6a3b049b472fa35efd071186
Richard Tinner <richardtinner@yahoo.co.uk>
2025-01-03 08:41:12 +0000

    Brute force attempt at day19 part 2. It doesn't end well.

--- day19.py
+++ day19.py
@@ -12,6 +12,20 @@ def check_design(design):
     
     return 0
 
+def check_design2(design, sum):
+    for t in towels:
+        if design.startswith(t):
+            if len(t) == len(design):
+                if sum % 10000 == 0:
+                    print('.',end='')
+                if sum % 100000 == 0:
+                    print(sum)
+                return sum+1
+            else:
+                sum = check_design2(design[len(t):], sum)
+
+    return sum
+
 with open("day19-data.txt") as f:
     lines = f.readlines()
     towels = [t.strip(' ') for t in lines[0].strip('\n').split(',')]
@@ -28,6 +42,16 @@ with open("day19-data.txt") as f:
             print("impossible")
     
     print("Day 19 part 1 = ", sum)
+    sum = 0
+    for d in designs:
+        print("Checking ", d, end=' ')
+        found = check_design2(d, 0)
+        if found != 0:
+            sum += found
+            print("\nfound ", found)
+        else:
+            print("impossible")
+    print("Day 19 part 2 = ", sum)
 
 
 
